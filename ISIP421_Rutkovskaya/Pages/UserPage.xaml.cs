b8a7c7264ef46a06b6969628e50848edc50a3110
Валерия Рутковская <217555@edu.fa.ru>
2024-11-27 13:59:42 +0300

    Занятия 10-12. Нужна проверка всего.

--- ISIP421_Rutkovskaya/Pages/UserPage.xaml.cs
+++ ISIP421_Rutkovskaya/Pages/UserPage.xaml.cs
@@ -23,6 +23,55 @@ namespace ISIP421_Rutkovskaya.Pages
 		public UserPage()
 		{
 			InitializeComponent();
+			SortComboBox.SelectedIndex = 0;
+			RoleCheckBox.IsChecked = false;
+
+			var currentUsers = Entities.GetContext().User.ToList();
+			ListUser.ItemsSource = currentUsers;
+		}
+
+		private void UpdateUsers()
+		{
+			//загружаем всех пользователей в список
+			var currentUsers = Entities.GetContext().User.ToList();
+			//осуществляем поиск по Ф.И.О. без учета регистра букв
+			currentUsers = currentUsers.Where(x => x.FIO.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
+			//обрабатываем фильтр по одной роли пользователей
+			if (RoleCheckBox.IsChecked.Value)
+				currentUsers = currentUsers.Where(x => x.Role.Contains("Пользователь")).ToList();
+			//осуществляем сортировку в зависимости от выбора пользователя
+			if (SortComboBox.SelectedIndex == 0)
+				ListUser.ItemsSource = currentUsers.OrderBy(x => x.FIO).ToList();
+			else
+				ListUser.ItemsSource = currentUsers.OrderByDescending(x => x.FIO).ToList();
+		}
+
+		private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			UpdateUsers();
+		}
+
+		private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			UpdateUsers();
+		}
+
+		private void RoleCheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			UpdateUsers();
+		}
+
+		private void RoleCheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			UpdateUsers();
+		}
+
+		private void ClearFilterButton_Click(object sender, RoutedEventArgs e)
+		{
+			SearchTextBox.Text = string.Empty;
+			SortComboBox.SelectedIndex = 0;
+			RoleCheckBox.IsChecked = false;
+			UpdateUsers();
 		}
 	}
 }
